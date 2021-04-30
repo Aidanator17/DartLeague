9986316063537b38ef28e83d65c82df2f7bd043c
Aidan Christopher <gandamovies@gmail.com>
2021-04-29 19:39:59 -0700

    who knows anymore

--- models/tourneyDatabase.js
+++ models/tourneyDatabase.js
@@ -1,7 +1,9 @@
 let sites = ['https://robsonlinedarts.herokuapp.com', 'http://localhost:8000']
 const request = require('request');
+const fetch = require('node-fetch');
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
+let sitenum = 1
 
 let tournaments = [{
     id: 1,
@@ -40,29 +42,33 @@ const tourneyModel = {
         })
     },
     enroll: async (t_id, currentuser) => {
-        request(sites[1]+'/db/tourneydb', function (error, response, body) {
+        var database = []
+        fetch(sites[sitenum] + '/db/tourneydb').then(function (res) {
+            return res.text();
+        }).then(async function (body) {
             database[0] = JSON.parse(body)
-      })
-      let x
-      for (tourney in database[0]){
-          if (database[0][tourney].id == t_id){
-              x = database[0][tourney]
-          }
-      }
-      x.enrolled.push(currentuser.id)
-      x.scores.push({
-        name:currentuser.name,
-        wins:0,
-        losses:0
-    })
-        const update = await prisma.tournament.update({
-            where: {
-                id: t_id,
-            },
-            data: {
-                enrolled: x.enrolled,
-                scores: x.scores
-            },
+            let x
+            for (tourney in database[0]) {
+                if (database[0][tourney].id == t_id) {
+                    x = database[0][tourney]
+                }
+            }
+            x.enrolled.push(currentuser.id)
+            x.scores.push({
+                name: currentuser.name,
+                wins: 0,
+                losses: 0
+            })
+            console.log(x)
+            const update = await prisma.tournament.update({
+                where: {
+                    id: t_id,
+                },
+                data: {
+                    enrolled: x.enrolled,
+                    scores: x.scores
+                },
+            })
         })
     },
 }
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -6,6 +6,7 @@ let tournaments = require("../models/tourneyDatabase").database
 let userModel = require("../models/userDatabase").userModel
 let tourneyModel = require("../models/tourneyDatabase").tourneyModel
 const request = require('request');
+const fetch = require('node-fetch');
 let sites = ['https://robsonlinedarts.herokuapp.com', 'http://localhost:8000']
 let sitenum = 1
 
@@ -24,61 +25,77 @@ router.get("/home", ensureAuthenticated, (req, res) => {
 })
 
 router.get("/tournaments", ensureAuthenticated, (req, res) => {
-    request(sites[sitenum] + '/db/tourneydb', function (error, response, body) {
+    fetch(sites[sitenum] + '/db/tourneydb').then(function (res) {
+        return res.text();
+    }).then(function (body) {
         tournaments[0] = JSON.parse(body)
+        fetch(sites[sitenum] + '/db/usersdb').then(function (res) {
+            return res.text();
+        }).then(function (body) {
+            users[0] = JSON.parse(body)
+            let currentuser
+            for (person in users[0]) {
+                if (req.user.id == users[0][person].id) {
+                    currentuser = users[0][person]
+                }
+            }
+            let tournamentexport = tournaments[0]
+            res.render("tournaments", { tournaments: tournamentexport, currentuser })
+        })
+
     })
-    request(sites[sitenum] + '/db/usersdb', function (error, response, body) {
-        users[0] = JSON.parse(body)
-    })
-    let currentuser
-    for (person in users[0]) {
-        if (req.user.id == users[0][person].id) {
-            currentuser = users[0][person]
-        }
-    }
-    let tournamentexport = tournaments[0]
-    res.render("tournaments", { tournaments: tournamentexport, currentuser })
+
 })
 
 router.get("/tournaments/:id", ensureAuthenticated, (req, res) => {
-    request(sites[sitenum] + '/db/tourneydb', function (error, response, body) {
+    fetch(sites[sitenum] + '/db/tourneydb').then(function (res) {
+        return res.text();
+    }).then(function (body) {
         tournaments[0] = JSON.parse(body)
-    })
-    request(sites[sitenum] + '/db/usersdb', function (error, response, body) {
-        users[0] = JSON.parse(body)
-    })
-    let currentuser
-    for (person in users[0]) {
-        if (req.user.id == users[0][person].id) {
-            currentuser = users[0][person]
-        }
-    }
-    let reminderToFind = req.params.id;
-    let searchResult = tournaments[0].find(function (tourney) {
-        return tourney.id == reminderToFind;
-    });
+        fetch(sites[sitenum] + '/db/usersdb').then(function (res) {
+            return res.text();
+        }).then(function (body) {
+            users[0] = JSON.parse(body)
+            let currentuser
+            for (person in users[0]) {
+                if (req.user.id == users[0][person].id) {
+                    currentuser = users[0][person]
+                }
+            }
+            let reminderToFind = req.params.id;
+            let searchResult = tournaments[0].find(function (tourney) {
+                return tourney.id == reminderToFind;
+            });
 
-    res.render("single-tournament", { tournament: searchResult, currentuser, })
+            res.render("single-tournament", { tournament: searchResult, currentuser, })
+        })
+    })
+
 })
 
 router.get("/enroll/:id", ensureAuthenticated, (req, res) => {
-    request(sites[sitenum] + '/db/tourneydb', function (error, response, body) {
+    fetch(sites[sitenum] + '/db/tourneydb').then(function (res) {
+        return res.text();
+    }).then(function (body) {
         tournaments[0] = JSON.parse(body)
-    })
-    request(sites[sitenum] + '/db/usersdb', function (error, response, body) {
-        users[0] = JSON.parse(body)
-    })
-    let currentuser
-    for (person in users[0]) {
-        if (req.user.id == users[0][person].id) {
-            currentuser = users[0][person]
-        }
-    }
+        fetch(sites[sitenum] + '/db/usersdb').then(function (res) {
+            return res.text();
+        }).then(function (body) {
+            users[0] = JSON.parse(body)
+            let currentuser
+            for (person in users[0]) {
+                if (req.user.id == users[0][person].id) {
+                    currentuser = users[0][person]
+                }
+            }
 
-    tourneyModel.enroll(req.params.id, currentuser)
-    userModel.enroll(req.params.id, currentuser)
+            tourneyModel.enroll(req.params.id, currentuser)
+            userModel.enroll(req.params.id, currentuser)
 
-    res.redirect("/tournaments/" + req.params.id)
+            res.redirect("/tournaments/" + req.params.id)
+        })
+    })
+
 })
 
 router.get("/createtourney", ensureAuthenticated, (req, res) => {
@@ -106,38 +123,48 @@ router.post("/createtourney", ensureAuthenticated, (req, res) => {
 })
 
 router.get("/activate/:id", ensureAuthenticated, (req, res) => {
-    request(sites[sitenum] + '/db/tourneydb', function (error, response, body) {
+    fetch(sites[sitenum] + '/db/tourneydb').then(function (res) {
+        return res.text();
+    }).then(function (body) {
         tournaments[0] = JSON.parse(body)
-    })
-    request(sites[sitenum] + '/db/usersdb', function (error, response, body) {
-        users[0] = JSON.parse(body)
-    })
-    let currenttourney
-    for (tourney in tournaments[0]) {
-        if (req.params.id == tournaments[0][tourney].id) {
-            currenttourney = tournaments[0][tourney]
-        }
-    }
-    tourneyModel.activate(req.params.id)
+        fetch(sites[sitenum] + '/db/usersdb').then(function (res) {
+            return res.text();
+        }).then(function (body) {
+            users[0] = JSON.parse(body)
+            let currenttourney
+            for (tourney in tournaments[0]) {
+                if (req.params.id == tournaments[0][tourney].id) {
+                    currenttourney = tournaments[0][tourney]
+                }
+            }
+            tourneyModel.activate(req.params.id)
 
-    res.redirect("/tournaments/" + req.params.id)
+            res.redirect("/tournaments/" + req.params.id)
+        })
+    })
+
 })
 
 router.get("/deactivate/:id", ensureAuthenticated, (req, res) => {
-    request(sites[sitenum] + '/db/tourneydb', function (error, response, body) {
+    fetch(sites[sitenum] + '/db/tourneydb').then(function (res) {
+        return res.text();
+    }).then(function (body) {
         tournaments[0] = JSON.parse(body)
-    })
-    request(sites[sitenum] + '/db/usersdb', function (error, response, body) {
-        users[0] = JSON.parse(body)
-    })
-    let currenttourney
-    for (tourney in tournaments[0]) {
-        if (req.params.id == tournaments[0][tourney].id) {
-            currenttourney = tournaments[0][tourney]
-        }
-    }
-    tourneyModel.deactivate(req.params.id)
+        fetch(sites[sitenum] + '/db/usersdb').then(function (res) {
+            return res.text();
+        }).then(function (body) {
+            users[0] = JSON.parse(body)
+            let currenttourney
+            for (tourney in tournaments[0]) {
+                if (req.params.id == tournaments[0][tourney].id) {
+                    currenttourney = tournaments[0][tourney]
+                }
+            }
+            tourneyModel.deactivate(req.params.id)
 
-    res.redirect("/tournaments/" + req.params.id)
+            res.redirect("/tournaments/" + req.params.id)
+        })
+    })
+
 })
 module.exports = router
